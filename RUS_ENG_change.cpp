aea8cee5b323d8f437544496234645b3dbcd46ba
Petya228Kvas <167185030+Petya228Kvas@users.noreply.github.com>
2025-07-14 15:06:09 +0500

    Add files via upload

---
+++ RUS_ENG_change.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    const string rus = "йцукенгшщзхъфывапролджэячсмитьбю";
+    const string eng = "qwertyuiop[]asdfghjkl;'zxcvbnm,.";
+    string s;
+    getline(cin, s);
+    for (size_t i = 0; i <= s.length(); i++) {
+        size_t pos = eng.find(s[i]);
+        if (pos != string::npos)
+            s[i] = rus[pos];
+    }
+    cout << s;
+    
+}
